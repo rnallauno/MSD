2b9b8a82cff0e000bd9682391414fb4813d43d1b
rnallauno <rnalla@unomaha.edu>
2025-12-03 00:45:07 -0600

    Changes for PythonAnywhere

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-)c)jl$)sqxhbuzkj^8k21oic$crv&cv25snkdro3fbsi+$^+s1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com']
 
 
 # Application definition
@@ -120,12 +121,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# Folder where Django will collect static files for deployment (PythonAnywhere will serve from here)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Folder where your own static assets live during development
+# Keep this line only if you actually have a "static" folder in your project (you probably do)
 STATICFILES_DIRS = [BASE_DIR / "static"]
+
+# Media files (uploads)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-MEDIA_URL = "media/"
-MEDIA_ROOT = BASE_DIR / "media"
